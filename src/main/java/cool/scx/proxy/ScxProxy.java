7ce80868d68aa9382ee9661b911d6611f3c86390
scx567888 <scx567888@outlook.com>
2024-01-23 17:38:31 +0800

    init

---
+++ src/main/java/cool/scx/proxy/ScxProxy.java
@@ -0,0 +1,8 @@
+package cool.scx.proxy;
+
+public class ScxProxy {
+    public static void main(String[] args) {
+        
+        
+    }
+}
---
+++ src/test/java/cool/scx/proxy/test/ScxProxyTest.java
@@ -0,0 +1,16 @@
+package cool.scx.proxy.test;
+
+import org.testng.annotations.Test;
+
+public class ScxProxyTest {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    @Test
+    public static void test1() {
+
+    }
+
+}
